bddbeff3a3bf6c27b5c8b246c72f77dd61639b5e
diegocallegari96 <warsindustry@gmail.com>
2023-09-25 14:22:42 +0200

    restart project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
 import logo from './assets/logo-white.png'
+import NavBar from "./constants/NavBar/NavBar.jsx";
 
 function App() {
     return (
-        <div className="page-container">
+        <>
+            <div className="page-container">
             <img src={logo} alt="Company logo"/>
-            <h1>Begin hier met het maken van jouw blog-applicatie!</h1>
-        </div>
+                <NavBar/>
+            </div>
+
+
+        </>
     )
 }
 
