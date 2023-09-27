c697580e3da119a0d80d642ac051161c441f1d6b
diegocallegari96 <warsindustry@gmail.com>
2023-09-27 13:23:23 +0200

    started Form

--- src/pages/NewPosts/NewPosts.jsx
+++ src/pages/NewPosts/NewPosts.jsx
@@ -1,10 +1,51 @@
 import "./NewPosts.css"
 
+
 function NewPosts() {
     return(
-        <div className="newPosts">
-        <h1>Work in progress!</h1>
-        </div>
+        <form>
+            <label htmlFor="title-field">
+                Titel:
+                <input
+                    type="text"
+                    name="title"
+                    id="title-field"
+                />
+            </label>
+
+            <label htmlFor="subtitle-field">
+                Subtitel:
+                <input
+                    type="text"
+                    name="subtitle"
+                    id="subtitle-field"
+                />
+            </label>
+
+            <label htmlFor="author-field">
+                Auteur:
+                <input
+                    type="text"
+                    name="author"
+                    id="author-field"
+                />
+            </label>
+
+            <label htmlFor="message-field">
+                Blogpost:
+                <textarea
+                    id="message-field"
+                    rows="4"
+                    cols="40"
+                    placeholder="Schrijf hier je blog"
+                    name="message-content"
+                ></textarea>
+            </label>
+
+            <button type="submit">
+                Plaatsen
+            </button>
+        </form>
     )
 }
 
