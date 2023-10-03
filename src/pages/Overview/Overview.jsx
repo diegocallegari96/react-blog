4b8c8d7fd0ee285bb29167ad913e7c6923cd0fc6
diegocallegari96 <warsindustry@gmail.com>
2023-10-03 11:38:17 +0200

    added button to fetch posts

--- src/pages/Overview/Overview.jsx
+++ src/pages/Overview/Overview.jsx
@@ -8,44 +8,49 @@ import axios from "axios";
 
 
 
+
 function Overview() {
-
     const [blogPosts, setBlogPosts] = useState([]);
-    useEffect(() => {
-        axios
-            .get("http://localhost:3000/posts")
-            .then((response) => {
-                const data = response.data;
-                setBlogPosts(data)
-            });
+    const [showPosts, setShowPosts] = useState(false);
 
-    }, []);
+    const fetchBlogPosts = () => {
+            axios
+                .get("http://localhost:3000/posts")
+                .then((response) => {
+                    const data = response.data;
+                    setBlogPosts(data);
+                    setShowPosts(true);
+                });
+        }
 
-    console.log()
-    const renderBlogPosts = () => {
+        const renderBlogPosts = () => {
+            if (showPosts) {
+                return (
+                    <ul className="blog-post-list">
+                        {blogPosts.map((blogPosts) => (
+                            <li key={blogPosts.id} className="blog-post-item">
+                                <Link to={`/blogposts/${blogPosts.id}`}>
+                                    {blogPosts.title} ({blogPosts.author})
+                                </Link>
+                                <p>
+                                    {blogPosts.comments} reacties - {blogPosts.shares} keer gedeeld
+                                </p>
+                            </li>
+                        ))}
+                    </ul>
+                );
+            }
+            return null;
+        };
+
         return (
-            <ul className="blog-post-list">
-                {blogPosts.map((blogPosts) => (
-                    <li key={blogPosts.id} className="blog-post-item">
-                        <Link to={`/blogposts/${blogPosts.id}`}>
-                            {blogPosts.title} ({blogPosts.author})
-                        </Link>
-                        <p>
-                            {blogPosts.comments} reacties - {blogPosts.shares} keer gedeeld
-                        </p>
-                    </li>
-                ))}
-            </ul>
+            <div className="overview">
+                <h1>Blog Overzicht</h1>
+                <button type="button" onClick={fetchBlogPosts}>Haal posts op</button>
+                {renderBlogPosts()}
+            </div>
         );
-    };
-
-    return (
-        <div className="overview">
-            <h1>Blog Overzicht</h1>
-            {renderBlogPosts()}
-        </div>
-    );
-}
+    }
 
 
 export default Overview;
